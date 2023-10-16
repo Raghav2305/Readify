3ef1359acceba5353fdaec1be43adc5db1372b89
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2023-10-16 23:33:21 +0530

    done with books routes

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import Book  from '../models/book.js';
-import Author  from '../models/author.js';
+import Book from '../models/book.js';
+import Author from '../models/author.js';
 
 
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
@@ -16,13 +16,13 @@ export const bookRouter = express.Router();
 bookRouter.get('/', async (req, res) => {
 
     let query = Book.find()
-    if(req.query.title != null && req.query.title != ''){
+    if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
-    if(req.query.publishedBefore != null && req.query.publishedBefore != ''){
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
         query = query.lte('publishDate', req.query.publishedBefore)
     }
-    if(req.query.publishedAfter != null && req.query.publishedAfter != ''){
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
         query = query.gte('publishDate', req.query.publishedAfter)
     }
     try {
@@ -53,36 +53,110 @@ bookRouter.post('/', async (req, res) => {
 
     try {
         const newBook = await book.save()
-        res.redirect('books')
+        res.redirect(`books/${newBook.id}`)
     } catch (error) {
         renderNewPage(res, book, true)
     }
-  })
+})
 
-  
+bookRouter.get('/:id', async (req, res) => {
 
-  async function renderNewPage(res, book, hasError = false){
+    try {
+        const book = await Book.findById(req.params.id).populate('author').exec()
+
+        res.render('books/show', {
+            book: book
+        })
+    } catch  {
+        res.redirect('/')
+    }
+})
+
+bookRouter.get('/:id/edit', async(req, res) => {
+    try{
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    }catch{
+        res.redirect('/')
+    }
+})
+
+bookRouter.put('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+
+        if(req.body.cover != null && req.body.cover !== ''){
+            saveCover(book, req.body.cover)
+        }
+        await book.save()
+        res.redirect(`/books/${book.id}`)
+    } catch {
+        if(book != null){
+            renderEditPage(res, book, true)
+        }else{
+            res.redirect('/')
+        }
+    }
+
+})
+
+bookRouter.delete('/:id', async(req, res) => {
+    let book
+    try {
+     book = await Book.deleteOne({_id: req.params.id})
+     res.redirect("/books")
+    } catch  {
+        if(book != null){
+            res.render('/books/show', {
+                book: book,
+                errorMessage: 'Could not delete book'
+            })
+        }else{
+            res.redirect('/')
+        }
+        
+    }
+})
+
+
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, 'edit', hasError)
+}
+async function renderNewPage(res, book, hasError = false){
+    renderFormPage(res, book, 'new', hasError)
+}
+
+
+
+async function renderFormPage(res, book, form,  hasError = false) {
     try {
         const authors = await Author.find({})
         const params = {
             authors: authors,
             book: book
-       }
+        }
 
-       if(hasError) params.errorMessage = "Error creating book"
-        res.render('books/new', params )
+        if (hasError) params.errorMessage = "Error creating book"
+        res.render(`books/${form}`, params)
     } catch {
         res.redirect('/books')
     }
-  }
+}
 
-  function saveCover(book, coverEncoded){
-    if(coverEncoded == null) return
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
 
     const cover = JSON.parse(coverEncoded)
 
-    if(coverEncoded != null && imageMimeTypes.includes(cover.type)){
+    if (coverEncoded != null && imageMimeTypes.includes(cover.type)) {
         book.coverImage = new Buffer.from(cover.data, 'base64')
         book.coverImageType = cover.type
     }
-  }
+}
